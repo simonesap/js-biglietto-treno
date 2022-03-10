477be5b9528e375502b3297b379b7264d09b0894
Simone Saputo <simonesaputo0@gmail.com>
2022-03-10 16:05:11 +0100

    adj-2

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,7 @@
 /*
 Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
   1) prompt per chiedere i chilometri da percorrere in (numero) esempio: 10
+    1.5) convertire la stringa in numero formula number() o parseInt()
   2) prompt per chiedere l'età del passeggero in (numero)
 Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 il prezzo del biglietto è definito in base ai km (0.21 € al km)
@@ -15,40 +16,41 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 function ticket() {
     
-    let lunghezzaViaggio = prompt("Ciao, per favore determina la lunghezza del tuo viaggio ");
-    console.log(lunghezzaViaggio)
-    // document.getElementById(lunghezza_viaggio).innerHTML = `${lunghezzaViaggio} "km"`;
+    var lunghezzaViaggio = Number(prompt("Ciao, per favore determina la lunghezza del tuo viaggio"));
+    console.log(lunghezzaViaggio);
+    console.log(typeof(lunghezzaViaggio));
+    document.getElementById('lunghezzaViaggio').innerHTML = `Percorrerai ${lunghezzaViaggio} km`;
 
-    let etàUtente = prompt("Per favore, inserisci la tua età ");
-    console.log(etàUtente)
-    // document.getElementById(età_utente).innerHTML = etàUtente + " km";
+    var etaUtente = Number(prompt("Per favore, inserisci la tua età"));
+    console.log(etaUtente);
+    console.log(typeof(etaUtente));
+    document.getElementById('etaUtente').innerHTML = `L'età del passeggero è: ${etaUtente}`;
 
-    const tariffaEuroAlKm = 0.21;
+    let tariffaEuroAlKm = 0.21;
 
-    let prezzoFinitoAlKm = lunghezzaViaggio * tariffaEuroAlKm;
-    console.log(prezzoFinitoAlKm)
+    var prezzoFinitoAlKm = lunghezzaViaggio * tariffaEuroAlKm;
+    console.log(prezzoFinitoAlKm);
+    // document.getElementById('prezzoFinitoAlKm').innerHTML = `Il tuo ticket costerà: ${prezzoFinitoAlKm.toFixed(2)} euro`;
 
 
-    if (etàUtente < 18) {
+    if (etaUtente < 18) {
         
-        let discount_20 = (prezzoFinitoAlKm / 100) * 20;
+        var discount_20 = (prezzoFinitoAlKm / 100) * 20;
         var finalPrice = prezzoFinitoAlKm - discount_20;
-        console.log(finalPrice)
-        document.getElementById(sconto_20).innerHTML ="Extra sconto minorenni del 20% applicato: " + finalPrice;
-        return finalPrice.toFixed(2);
+        console.log(finalPrice);
+        document.getElementById('sconto_20').innerHTML =`Extra sconto minorenni del 20% applicato: ${finalPrice.toFixed(2)} euro`;
         
-    } else if (etàUtente > 65) {
+    } else if (etaUtente > 65) {
         
-        let discount_40 = (prezzoFinitoAlKm / 100) * 40;
+        var discount_40 = (prezzoFinitoAlKm / 100) * 40;
         var finalPrice = prezzoFinitoAlKm - discount_40;
         console.log(finalPrice)
-        document.getElementById(sconto_40).innerHTML = "Extra sconto over 65 del 40% applicato: " + finalPrice;
-        return finalPrice.toFixed(2);
+        document.getElementById('sconto_40').innerHTML = `Extra sconto over 65 del 40% applicato: ${finalPrice.toFixed(2)} euro`;
 
     } else {
-    
-        document.getElementById(prezzo_pieno).innerHTML = "Il tuo ticket costerà: " + prezzoFinitoAlKm;
-        return prezzo_pieno.toFixed(2);
+
+      document.getElementById('prezzo_normale').innerHTML = `Il tuo ticket costerà: ${prezzo_normale.toFixed(2)} euro`;
+
     }
     
 }
